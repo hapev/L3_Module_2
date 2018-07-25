4994140458de1fab633b14d31a5dd58b6938db7d
hapev <league@WTS13.attlocal.net>
2018-07-24 18:31:01 -0700

    finished module 2

--- src/extra/NumberFour.java
+++ src/extra/NumberFour.java
@@ -1,34 +1,47 @@
 package extra;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class NumberFour implements ActionListener {
-	JPanel panel;
+	JPanel panel1;
+	JPanel panel2;
 	JFrame frame;
 	JButton add;
 	JButton remove;
 	JButton save;
 	JButton load;
 	JButton selectedTask;
+	int height = 200;
+	int width = 720;
+	int whichTask = 0;
 
-	ArrayList<JButton> list;
+	public ArrayList<JButton> list;
 
 	public NumberFour() {
-		panel = new JPanel();
+		panel1 = new JPanel();
+		panel2 = new JPanel();
 		frame = new JFrame();
 		add = new JButton();
 		remove = new JButton();
 		save = new JButton();
 		load = new JButton();
 		list = new ArrayList<JButton>();
-
+		BorderLayout manager = new BorderLayout();
+		frame.setLayout(manager);
 		add.addActionListener(this);
 		remove.addActionListener(this);
 		save.addActionListener(this);
@@ -37,30 +50,35 @@ public class NumberFour implements ActionListener {
 
 	public void add() {
 		JButton J = new JButton(JOptionPane.showInputDialog("Type Your Task Here"));
+
 		frame.setVisible(false);
 		list.add(J);
 		J.addActionListener(this);
 		J.setText(J.getText());
-		panel.add(J);
+
+		panel2.add(J, BorderLayout.CENTER);
 		frame.pack();
 		frame.setVisible(true);
 
-		
 	}
 
 	public void run() {
-		frame.add(panel);
-		
-		panel.add(add);
-		panel.add(remove);
-		panel.add(load);
-		panel.add(save);
+		frame.add(panel1, BorderLayout.NORTH);
+		frame.add(panel2, BorderLayout.SOUTH);
+		panel1.add(add);
+		panel1.add(remove);
+		panel1.add(load);
+		panel1.add(save);
 		load.setText("load");
 		save.setText("save");
 		remove.setText("remove");
 		add.setText("add");
+		load();
 		frame.pack();
+
 		frame.setVisible(true);
+		
+
 
 	}
 
@@ -78,22 +96,94 @@ public class NumberFour implements ActionListener {
 		if (e.getSource() == remove) {
 			remove();
 		}
-		if (e.getSource()!=add||e.getSource()!=remove||e.getSource()!=load||e.getSource()!=save) {
-			selectedTask = (JButton)e.getSource();
+		if (e.getSource() == save) {
+			write();
+		}
+		if (e.getSource() == load) {
+			load();
+		}
+		
+		
+		if (e.getSource() != add && e.getSource() != remove && e.getSource() != load && e.getSource() != save) {
+			selectedTask = (JButton) e.getSource();
 		}
 
 	}
 
 	public void remove() {
-		if (selectedTask !=null) {
-		JButton j = selectedTask;
-		frame.setVisible(false);
-		panel.remove(j);
-		list.remove(j);
-		frame.pack();
-		frame.setVisible(true);
-		
+		if (selectedTask != null) {
+			JButton j = selectedTask;
+			frame.setVisible(false);
+			panel2.remove(j);
+			list.remove(j);
+			frame.pack();
+			frame.setVisible(true);
+
 		}
 	}
 
+	public void write() {
+		try {
+			JFileChooser chooser = new JFileChooser();
+			chooser.showOpenDialog(null);
+			FileWriter fWriter = new FileWriter(chooser.getSelectedFile());
+			BufferedWriter bWriter = new BufferedWriter(fWriter);
+			for (JButton J:list) {
+				bWriter.write(J.getText()+"\n");
+				
+				
+			}
+			bWriter.flush();
+			
+			bWriter.close();
+			fWriter.close();
+		
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		}
+		public void load() {
+			
+			for (JButton J:list) {
+				list.remove(J);
+				panel2.remove(J);
+			
+			}
+			
+			
+				
+				
+				try {
+				JFileChooser chooser = new JFileChooser();
+				chooser.showOpenDialog(null);
+				FileReader fReader = new FileReader(chooser.getSelectedFile());
+				BufferedReader bReader = new BufferedReader(fReader);
+		
+				
+				String read = bReader.readLine();
+				while (read != null) {
+					JButton J = new JButton(read);
+					frame.setVisible(false);
+					list.add(J);
+					J.addActionListener(this);
+					J.setText(J.getText());
+					panel2.add(J, BorderLayout.CENTER);
+					frame.pack();
+					frame.setVisible(true);
+					read = bReader.readLine();
+
+				}
+				fReader.close();
+				bReader.close();
+
+				
+
+				
+			
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
 }
